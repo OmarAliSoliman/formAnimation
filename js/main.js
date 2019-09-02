186f04b9a81a52d250714db40fb3c2f5722b0afe
OmarAliSoliman <49602133+OmarAliSoliman@users.noreply.github.com>
2019-09-03 00:38:39 +0200

    formAnimation

--- js/main.js
+++ js/main.js
@@ -1,68 +1,109 @@
+const msg = document.getElementById('msg');
+const submit = document.querySelector('#submit');
+const helloMsg = document.getElementById('helloMsg');
+const heart = document.querySelectorAll('.fa-heart');
+
 function animationForm(){
-    const arrow = document.querySelectorAll('.fa-arrow-down');
-    console.log(arrow);
+    const arrows = document.querySelectorAll('.fa-arrow-down');
 
-    arrow.forEach(arow=>{
-        arow.addEventListener('click', ()=>{
-            const input = arow.previousElementSibling;
-            const parent = arow.parentElement;
+    arrows.forEach(arrow =>{
+        arrow.addEventListener('click',()=>{
+            const input = arrow.previousElementSibling;
+            const parent = arrow.parentElement;
             const next = parent.nextElementSibling;
-
-            if(input.type==="text" && validateName(input)){
-                nextSlider(parent, next);
-            }else if(input.type==="email" && validateEmail(input)){
-                nextSlider(parent, next);
-            }else if(input.type==="password" && validatePassword(input)){
-                nextSlider(parent, next);
-            }else{
-                parent.style.animation = "shake 0.5s ease";
+            const name = arrows[0].previousElementSibling.value;
+            
+            if(input.type==="text" && validateName(input, parent)){
+                nextSlider(parent, next, "0", name);
+            }else if(input.type === "email" && validateEmail(input, parent)){
+                nextSlider(parent, next, "0", name);
+            }else if(input.type === "password" && validatePassword(input, parent)){
+                nextSlider(parent, next, "-1", name);
             }
-            parent.addEventListener('animationend',()=>{
-                parent.style.animation="";
-            })
         })
     })
 }
 
-function validateName(user){
-    if(user.value.length < 6){
-        error(" rgb(189, 87, 87)");
+function validateName(user, parent){
+    if(user.value.length < 4){
+        error("rgb(189, 87, 87)");
+        animationError(parent);
+        getMsgError("Name must be grater that 4 chachter")
     }else{
-        error(" rgb(87, 189, 130)");
+        error("rgb(87, 189, 130)");
+        getMsgError("");
         return true;
     }
 }
 
-function validateEmail(email){
+function validateEmail(email, parent){
     const validate = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if(validate.test(email.value)){
-        error(" rgb(87, 189, 130)");
+        error("rgb(87, 189, 130)");
+        getMsgError("");
         return true;
     }else{
-        error(" rgb(189, 87, 87)");
+        error("rgb(189, 87, 87)");
+        animationError(parent);
+        getMsgError("Please Enter Validte Email");
     }
 }
 
-function validatePassword(password){
-    if(password.value.length<5){
-        error(" rgb(189, 87, 87)");
+function validatePassword(password, parent){
+    if(password.value.length < 5){
+        error("rgb(189, 87, 87)");
+        animationError(parent);
+        getMsgError("The password must be grater that 5 chachter")
     }else{
-        error(" rgb(87, 189, 130)");
+        error("rgb(87, 189, 130)");
+        getMsgError("");
         return true;
     }
-
 }
 
 function error(color){
     document.body.style.backgroundColor = color;
 }
 
-function nextSlider(parent, next){
-    parent.classList.add('innactive');
-    parent.classList.remove('active');
-    next.classList.add('active');
-    next.classList.remove('innactive');
+function getMsgError(message){
+    msg.innerHTML = message;
+    msg.style.transition="all 1s ease"
+    msg.style.opacity="1";
 }
 
+function nextSlider(parent, next, cheak, name){
+    if(cheak=="-1"){
+        submitForm(name);
+    }
+    next.classList.remove("innactive");
+    parent.classList.add("active");
+}
+
+function animationError(parent){
+    parent.style.animation = "shake 0.5s ease";
+    parent.addEventListener('animationend', function(){
+        parent.style.animation = "";
+    })
+
+}
+
+function submitForm(name){
+    const parentSubmit = submit.parentElement;
+    const nextSubmit = parentSubmit.nextElementSibling;
+    submit.addEventListener('click', function(e){
+        e.preventDefault();
+        console.log(name);
+        nextSubmit.classList.remove("innactive");
+        parentSubmit.classList.add('innactive');
+        if(name === "sarah" || name === "sara" || name==="suty" || name === "Sarah" || name === "Sara" || name === "Suty"){
+            helloMsg.innerHTML = " Suty. you are in my heart ^_^";
+            heart.forEach(hr =>{
+                hr.style.color = "red";
+            })
+        }else{
+            helloMsg.innerHTML = "Thank You" + ` ${name}`;
+        }
+    });
+}
 
 animationForm();
